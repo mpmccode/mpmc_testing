e40feb357d6b50b3857946f370b1bdc8ae1f26ee
L <luciano@laratelli.com>
2018-09-11 23:27:11 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,22 @@
+# MPMC Tests
+This directory contains the tests, test inputs, and test runner (`test.py`) for the MPMC program. The only prerequisites to running the tests are python 2.7 and the `termcolor` library.
+
+## How to write tests
+Each test needs three files:
+
+ - A *description file*, in the `tests/` directory, 
+ - An *MPMC input file*, in the `inputs/` directory, and
+ - A *pqr file*, also in the `inputs/` directory.
+ 
+ ### Description Files 
+ The following would be found in `tests/example.txt`:
+ 
+    name example
+    input example.inp
+    pqr example.pqr
+    term OUTPUT: example energy
+    output 12345.6789
+    precision exact
+Note that a `precision` of `exact` is the same as setting the precision to `0.0`. `example.inp` and `example.pqr` are both in the `inputs/` directory, as noted above.
+### Input/PQR Files
+If you're writing tests for MPMC, you should know what these are. Nonetheless, you can look at examples in `sample_configs/`  contained in the main `mpmc` directory if you want some inspiration.
---
+++ test.py
@@ -0,0 +1,109 @@
+'''
+Runs MPMC tests as defined in the README
+'''
+
+import os
+import os.path
+import re
+from subprocess import check_output
+from termcolor import colored
+
+
+class Test():
+    def __init__(self):
+        self.name = ""
+        self.input_file = ""
+        self.pqr = ""
+        self.search_string = ""
+        self.expected_result = None
+        self.precision = None
+        #self.ok = None
+
+
+'''
+Get our params and strip newlines wherever they broke other code
+'''
+
+
+def read_test_parameters():
+    tests = []
+    tests_dir = 'tests'
+    for test in os.listdir(tests_dir):
+        path = tests_dir + "/" + test
+        temp_test = Test()
+        for line in open(path, "r"):
+            if line[0] == '#':
+                continue
+            if re.search("name", line):
+                temp_test.name = line.split(' ', 1)[1].strip()
+            elif re.search("input", line):
+                temp_test.input_file = line.split(' ', 1)[1].strip()
+            elif re.search("pqr", line):
+                temp_test.pqr = line.split(' ', 1)[1]
+            elif re.search("term", line):
+                temp_test.search_string = line.split(' ', 1)[1].strip()
+            elif re.search("output", line):
+                temp_test.expected_result = line.split(' ', 1)[1].strip()
+            elif re.search("precision", line):
+                temp_test.precision = line.split(' ', 1)[1].strip()
+            else:
+                print "Found unsupported option in file ", path
+                print line
+        tests.append(temp_test)
+    return tests
+
+
+def check_result(test, answer):
+    precision = test.precision
+    expected = test.expected_result
+    if precision == "exact":
+        precision = 0.0
+
+    if abs(float(answer) - float(expected)) <= precision:
+        output = "Test " + test.name.strip() + " passed!"
+        print colored(output, 'green')
+    else:
+        output = "Test " + test.name + " failed.\n"
+        output += "Expected answer: " + test.expected_result + " with precision of " + str(
+                precision) + "\n"
+        output += "Actual answer: " + answer
+
+        print colored(output, 'red')
+
+
+'''
+Run the tests. We choose only the first match of a search string and find the
+first floating point number after the match as result due to how MPMC is built: we can't
+run 0 steps (i.e, output the initial system without outputting the first move after.)
+'''
+
+
+def run_test(test):
+    #TODO: generate these paths dynamically
+    mpmc_exe = '../../build/mpmc' #two directories up because when we cd down we're one dir further from the exe
+    test_dir = 'inputs/'
+    input_file = test.input_file
+    cwd = os.getcwd()
+    os.chdir(test_dir)
+    out = check_output([mpmc_exe, input_file])
+    out = out.decode("ascii", errors="ignore")
+    numeric_const_pattern = '[-+]? (?: (?: \d* \. \d+ ) | (?: \d+ \.? ) )(?: [Ee] [+-]? \d+ ) ?'
+    rx = re.compile(numeric_const_pattern, re.VERBOSE)
+    for line in out.splitlines():
+        term = test.search_string
+        if term in line:
+            result = rx.findall(line)[0]
+            break
+    check_result(test, result)
+    os.chdir(cwd)
+
+   #rm *.dat *final* *restart* *traj*
+
+def main():
+    tests = read_test_parameters()
+    for test in tests:
+        run_test(test)
+
+
+if __name__ == '__main__':
+    main()
