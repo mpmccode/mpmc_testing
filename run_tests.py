02ab7d71455cee1e70ee6954379eb9d63492603e
L <luciano@laratelli.com>
2018-09-24 23:40:16 -0400

    empty commit message

--- run_tests.py
+++ run_tests.py
@@ -1,15 +1,15 @@
-'''
+"""
 Runs MPMC tests as defined in the README
-'''
+"""
 
 import os
 import os.path
 import re
 import subprocess
-import pdb
+import sys
 
 
-class Test():
+class Test:
     def __init__(self):
         self.name = ""
         self.input_file = ""
@@ -20,9 +20,9 @@ class Test():
         self.search = ""
 
 
-'''
+"""
 Get our params and strip newlines wherever they broke other code 
-'''
+"""
 
 
 def read_test_parameters():
@@ -49,45 +49,46 @@ def read_test_parameters():
             elif re.search("search", line):
                 temp_test.search = line.split(' ', 1)[1].strip()
             else:
-                print "Found unsupported option in file ", path
-                print line
+                print("Found unsupported option in file {}".format(path))
+                print(line)
         tests.append(temp_test)
     return tests
 
 
 def test_passed(test):
-    CGREEN = '\33[32m'
-    CEND = '\033[0m'
+    cgreen = '\33[32m'
+    cend = '\033[0m'
     output = "Test " + test.name.strip() + " passed!"
-    print CGREEN + output + CEND
+    print(cgreen + output + cend)
 
 
 def test_failed(test, answer):
-    CRED = '\033[91m'
-    CEND = '\033[0m'
+    cred = '\033[91m'
+    cend = '\033[0m'
     if test.precision == "greater" or test.precision == "less":
         output = "Test " + test.name + " failed.\n"
         output += "Expected answer: " + test.expected_result + "\n"
         output += "Actual answer: " + answer
-        print CRED + output + CEND
+        print(cred + output + cend)
     else:
         output = "Test " + test.name + " failed.\n"
         output += "Expected answer: " + test.expected_result + " with precision of " + str(
             test.precision) + "\n"
         output += "Actual answer: " + answer
-        print CRED + output + CEND
+        print(cred + output + cend)
 
 
 def check_result(test, answer):
+    precision = None
     if test.precision == "exact":
         precision = 0.0
     elif test.precision == "less":
-        if answer < expected_result:
+        if answer < test.expected_result:
             test_passed(test)
         else:
             test_failed(test, answer)
     elif test.precision == "greater":
-        if answer > expected_result:
+        if answer > test.expected_result:
             test_passed(test)
         else:
             test_failed(test, answer)
@@ -96,8 +97,6 @@ def check_result(test, answer):
 
     if abs(float(answer) - float(test.expected_result)) <= precision:
         test_passed(test)
-    elif str(test.expected_result) == "FAIL_SUBPROCESS":
-        print CRED + "Test failed due to subprocess error-- is mpmc_testing in the right directory?" + CEND
     else:
         test_failed(test, answer)
 
@@ -115,36 +114,38 @@ def run_once(f):
 @run_once
 def check_mpmc_exists(executable):
     if os.path.isfile(os.path.realpath(executable)):
-        print "MPMC executable found, continuing..."
+        print("MPMC executable found, continuing...")
     else:
-        print "MPMC executable not found, halting program execution"
+        print("MPMC executable not found, halting program execution")
+        sys.exit()
 
 
-#Run the tests. We choose only the first match of a search string and find the
-#first floating point number after the match as result due to how MPMC is built: we can't
-#run 0 steps (i.e, output the initial system without outputting the first move after.)
+# Run the tests. We choose only the first match of a search string and find the
+# first floating point number after the match as result due to how MPMC is built: we can't
+# run 0 steps (i.e, output the initial system without outputting the first move after.)
 
 
 def run_test(test):
-    #TODO: generate these paths dynamically
+    # TODO: generate these paths dynamically
     test_dir = 'inputs'
     input_file = test.input_file
     cwd = os.getcwd()
     os.chdir(test_dir)
     mpmc_exe = '../../build/mpmc'
-    check_mpmc_exists(mpmc_exe)  #exit here if MPMC executable provided is not correct
+    check_mpmc_exists(mpmc_exe)  # exit here if MPMC executable provided is not correct
     try:
         out = subprocess.check_output([mpmc_exe, input_file])
     except:
-        test.expected_result = "FAIL_SUBPROCESS"
-        return
+        print("subprocess returned an error, exiting")
+        sys.exit()
 
     out = out.decode("ascii", errors="ignore")
-    #stole this next line from SO, I can't read regex yet so all I know is it gets the numbers from the goop
+    # stole this next line from SO, I can't read regex yet so all I know is it gets the numbers from the goop
     numeric_const_pattern = '[-+]? (?: (?: \d* \. \d+ ) | (?: \d+ \.? ) )(?: [Ee] [+-]? \d+ ) ?'
-    #probably doesn't need to be done each time this function is called
+    # probably doesn't need to be done each time this function is called
     rx = re.compile(numeric_const_pattern, re.VERBOSE)
     output = out.splitlines()
+    result = None
     if test.search == "reverse":
         output.reverse()
     for line in output:
@@ -156,13 +157,13 @@ def run_test(test):
     os.chdir(cwd)
 
 
-#remove garbage in case the test writer forgets to redirect MPMC output to /dev/null in their input script
+# remove garbage in case the test writer forgets to redirect MPMC output to /dev/null in their input script
 def cleanup():
     test_dir = 'inputs/'
     cwd = os.getcwd()
     os.chdir(test_dir)
-    #using shell=True is a bit dangerous here, but it's the only way (as far as I can tell) to
-    #have subprocess evaluate wildcards properly -L
+    # using shell=True is a bit dangerous here, but it's the only way (as far as I can tell) to
+    # have subprocess evaluate wildcards properly -L
     subprocess.call(
         "rm *.dat *.last *.restart.* *.traj* *.energy* *.final*",
         shell=True,
@@ -175,7 +176,7 @@ def main():
     tests = read_test_parameters()
     for test in tests:
         run_test(test)
-    cleanup()  #clean up, everybody clean up!
+    cleanup()  # clean up, everybody clean up!
 
 
 if __name__ == '__main__':
