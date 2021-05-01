838893bc1f38f1fe15742a2e50b48dda24b37322
Adam Hogan <purplepanzer1@gmail.com>
2021-05-01 15:47:52 -0400

    bug fixes for all_commits

--- test_all_commits.sh
+++ test_all_commits.sh
@@ -1,21 +1,24 @@
-module load compilers/intel/2018_cluster_xe
-module load apps/python/3.6.2
+#!/bin/bash
+rm test_results.txt
 for commit in $(cat mpmc_commits.txt); do
   git clone git@github.com:mpmccode/mpmc.git ${commit}
   cd ${commit}
   git checkout -b ${commit} ${commit}
   if [ -f CMakeLists.txt ];then
-    echo ${commit} >> ~/test_results.txt
     rm -rf mpmc_testing
-    git clone    git@github.com:LucianoLaratelli/mpmc_testing.git
-    bash compile.sh
+    git clone    git@github.com:mpmccode/mpmc_testing.git
+    mkdir build
+    cd build
+    cmake -DQM_ROTATION=OFF -DVDW=OFF -DMPI=OFF -DOPENCL=OFF -DCUDA=OFF -DCMAKE_BUILD_TYPE=Release -Wno-dev ../
+    make
+    cd ..
     cd mpmc_testing
-    ./run_tests.py >> ~/test_results.txt
+    echo ${commit} >> ../../test_results.txt
+    ./run_tests.py >> ../../test_results.txt
   else
     continue
   fi
-  cd ..
-  rm -rf ${commit}
-  exit
+  cd ../..
+  #rm -rf ${commit}
 done
 
